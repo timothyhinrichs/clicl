6e09a7b1cb79dbae2c03843d7f7bd75bb1d07ac8
thinrich <thinrich@00acf523-aa1e-78f6-8fce-79436e7c7f26>
2010-07-19 21:31:33 +0000

    Missed a file during reorg.
    git-svn-id: https://clicl.googlecode.com/svn/code@428 00acf523-aa1e-78f6-8fce-79436e7c7f26

---
+++ clicl/compiler.lisp
@@ -0,0 +1,13 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; (c) Copyright 1999-2007 by Michael Genesereth.  All rights reserved.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; to compile, load this file
+
+(load (loadfn "compiler" :dir "epilog"))
+(load (loadfn "compiler" :dir "infomaster"))
+(load (loadfn "compiler" :dir "researchmaster"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; (c) Copyright 1999-2007 by Michael Genesereth.  All rights reserved.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
