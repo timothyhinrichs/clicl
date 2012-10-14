76bde156f7833a68b1b3972643693082351cc78c
Gabriele Petronella <gabriele.petronella@gmail.com>
2012-10-14 16:06:27 -0500

    Changed message text from html to string

--- researchmaster/webapps/gabriele.lisp
+++ researchmaster/webapps/gabriele.lisp
@@ -1,9 +1,9 @@
 ; Signature for permanent storage of messages
-(defsignature3 db.message (id :type integer) author (text :type (integer html)))
+(defsignature3 db.message (id :type integer) author (text :type (integer string)))
 (defschema db.message :signature db.message :guards(message))
 
 ; Signature for showing and sending messages
-(defsignature message id (text :type html)
+(defsignature message (id :type integer) (text :type string))
 (defschema message :signature message :guards(message))
 
 ; message id must be unique
@@ -11,17 +11,20 @@
   (=> (message.id ?x) (message.id ?y) (= ?x ?y))
 )
 
+; save a new message to the db
 (defupdate savemessage (:language posneg)
   (<= (pos (db.message.id ?x)) (message.id ?x))
   (<= (pos (db.message.author ?x ?y)) (and (message.id ?x)(session.username ?y)))
   (<= (pos (db.message.text ?x ?y)) (and (message.id ?x)(message.text ?y)))
-  )
+ )
 
-
+; the form to input a new message
 (defform new-message :schema message :target new-message)
 
+; the table to show the messages
 (deftable messages :schema message)
 
+; //TODO insert guard loggedin
 (defservlet show-message-board :page message-board-page)
 (defservlet new-message :updates (savemessage) :page show-message-board)
 
